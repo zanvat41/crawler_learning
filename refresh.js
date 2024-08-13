b7f88e776839de3a23122d1793706b8519f63334
Zhe Lin <fslinzhe@hotmail.com>
2024-08-13 14:37:26 -0400

    check if page reloaded

--- refresh.js
+++ refresh.js
@@ -24,6 +24,15 @@ function reload() {
       if(elem1.length > 0) beep();
     });
     round ++;
+    if (window.performance) {
+      console.info("window.performance works fine on this browser");
+    }
+    console.info(performance.navigation.type);
+    if (performance.navigation.type == performance.navigation.TYPE_RELOAD) {
+      console.info( "This page is reloaded" );
+    } else {
+      console.info( "This page is not reloaded");
+    }
     void(close());
   }
 }
